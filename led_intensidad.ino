8ee6d5621e2fc42e1b42979e4564a292c10fa365
Caftree24 <105188496+Caftree24@users.noreply.github.com>
2024-01-17 22:04:42 -0600

    led_intensidad.ino

---
+++ led_intensidad.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0; i<255; i++){
+    analogWrite(5,i);// 0 - 255
+    delay(20);
+  }
+   for(int i=255; i>0; i--){
+    analogWrite(5,i);// 0 - 255
+    delay(20);
+  }
+  
+
+}
